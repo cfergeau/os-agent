d5c2a91cce3298d1d98137bb647a60e89b8c2b49
Christophe Fergeau <cfergeau@redhat.com>
2023-12-11 15:38:11 +0100

    led: Add SetTrigger() test
    This cannot use the preexisting `testdata` as this would rewrite the
    `trigger` files it contain, and some modified files would show up in git
    after running the test suite.
    We use an empty temporary `/sys/class/leds` location instead, and check
    the written files have the content we expect.

--- utils/led/led_test.go
+++ utils/led/led_test.go
@@ -1,6 +1,7 @@
 package led
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -19,4 +20,29 @@ func TestGetTrigger(t *testing.T) {
 	testGetTriggerForLed(t, "activity", "activity")
 	testGetTriggerForLed(t, "power", "default-on")
 	testGetTriggerForLed(t, "user", "heartbeat")
+}
+
+func requireEqualFileContent(t *testing.T, path, content string) {
+	data, err := os.ReadFile(path)
+	require.NoError(t, err)
+	require.Equal(t, content, string(data))
+}
+
+func TestSetTrigger(t *testing.T) {
+	sysClassLedsPath = t.TempDir()
+	ledPower := LED{Name: "power", DefaultTrigger: "default-on"}
+	err := os.Mkdir(ledPower.getSysfsPath(), 0755)
+	require.NoError(t, err)
+
+	err = ledPower.SetTrigger(false)
+	require.NoError(t, err)
+
+	ledTriggerFilePath := ledPower.getSysfsPath("trigger")
+	requireEqualFileContent(t, ledTriggerFilePath, "none")
+
+	err = ledPower.SetTrigger(true)
+	require.NoError(t, err)
+
+	requireEqualFileContent(t, ledTriggerFilePath, "default-on")
+
 }
